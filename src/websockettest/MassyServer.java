0b07aec788525e1f821d0df78a614103176c6d44
Sergey <therealmasel@gmail.com>
2012-06-02 22:13:06 -0700

    Initial import.

---
+++ src/websockettest/MassyServer.java
@@ -0,0 +1,23 @@
+package com.smaslov.websockettest;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.DefaultHandler;
+
+public class MassyServer {
+
+	public static void main(String[] args) {
+		Server server = new Server (8081);
+		MassyWebSocketHandler socketHandler = new
+				MassyWebSocketHandler();
+		socketHandler.setHandler(new DefaultHandler());
+		server.setHandler(socketHandler);
+		try {
+			server.start();
+			server.join();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+}
